bfbbed9eddf77cba26d60f92bc4afc11880671ed
Renan Valladão <renancvalladao@gmail.com>
2022-01-08 22:59:09 -0300

    Adding title and search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 
 function App() {
+
+  const [searchText, setSearchText] = useState('')
+
+  const handleSearch = () => {
+    console.log('Searching...')
+  }
+
   return (
-    <div>
+    <div className='app'>
+      <h1 className='title'>Book Finder</h1>
+      <div>
+        <input
+          className='query-input'
+          type="text"
+          placeholder='Search by author, book name...'
+          spellCheck={false}
+          autoComplete="off"
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+          onKeyPress={e => {
+            if (e.key === 'Enter') handleSearch()
+          }} />
+          <FontAwesomeIcon className='search-icon' icon={faSearch} onClick={handleSearch}/>
+      </div>
     </div>
   );
 }
