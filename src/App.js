bfa31155ce5f18467590b51bcb9cadec7a6d44d6
Renan Valladão <renancvalladao@gmail.com>
2022-01-09 20:08:57 -0300

    Using BookCard component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
+import BookCard from './components/BookCard'
 
 function App() {
 
   const [searchText, setSearchText] = useState('')
+  const [totalItems, setTotalItems] = useState(-1)
+  const [books, setBooks] = useState([])
 
   const handleSearch = () => {
-    console.log('Searching...')
+    if (searchText !== '') {
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchText}&maxResults=12`)
+        .then(resp => resp.json())
+        .then(data => {
+          setTotalItems(data.totalItems)
+          setBooks(data.items)
+        })
+    }
   }
 
   return (
     <div className='app'>
       <h1 className='title'>Book Finder</h1>
-      <div>
+      <div className='search-container'>
         <input
           className='query-input'
           type="text"
@@ -26,7 +36,11 @@ function App() {
           onKeyPress={e => {
             if (e.key === 'Enter') handleSearch()
           }} />
-          <FontAwesomeIcon className='search-icon' icon={faSearch} onClick={handleSearch}/>
+        <FontAwesomeIcon className='search-icon' icon={faSearch} onClick={handleSearch} />
+        <p className='books-found'>{totalItems >= 0 ? `${totalItems} book${totalItems > 1 ? 's' : ''} found` : ''}</p>
+      </div>
+      <div className='book-card-container'>
+        {books?.map(book => <BookCard key={book.id} book={book}></BookCard>)}
       </div>
     </div>
   );
