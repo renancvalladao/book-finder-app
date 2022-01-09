dd26ef84cffce981e9588220a9651cbc19c0b53e
Renan Valladão <renancvalladao@gmail.com>
2022-01-09 20:19:22 -0300

    Adding loading animation

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ function App() {
   const [searchText, setSearchText] = useState('')
   const [totalItems, setTotalItems] = useState(-1)
   const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const handleSearch = () => {
     if (searchText !== '') {
+      setLoading(true)
       fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchText}&maxResults=12`)
         .then(resp => resp.json())
         .then(data => {
           setTotalItems(data.totalItems)
           setBooks(data.items)
+          setLoading(false)
         })
     }
   }
@@ -37,10 +40,11 @@ function App() {
             if (e.key === 'Enter') handleSearch()
           }} />
         <FontAwesomeIcon className='search-icon' icon={faSearch} onClick={handleSearch} />
-        <p className='books-found'>{totalItems >= 0 ? `${totalItems} book${totalItems > 1 ? 's' : ''} found` : ''}</p>
+        <p className='books-found'>{!loading && totalItems >= 0 ? `${totalItems} book${totalItems !== 1 ? 's' : ''} found` : ''}</p>
       </div>
+      {loading && <div class="loader"></div>}
       <div className='book-card-container'>
-        {books?.map(book => <BookCard key={book.id} book={book}></BookCard>)}
+        {!loading && books?.map(book => <BookCard key={book.id} book={book}></BookCard>)}
       </div>
     </div>
   );
